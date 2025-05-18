5b7148b920d5e35d27866321c67cf3b74a286eba
Kapil Yadav <ykapil086@gmail.com>
2025-05-18 14:09:32 +1000

    Final project

--- public/js/script.js
+++ public/js/script.js
@@ -19,10 +19,10 @@
 
   // From index.ejs — move to public/js/script.js
 document.addEventListener("DOMContentLoaded", () => {
-  const taxSwitch = document.getElementById("switchCheckDefault");
+  let taxSwitch = document.getElementById("switchCheckDefault");
   if (taxSwitch) {
     taxSwitch.addEventListener("click", () => {
-      const taxInfo = document.getElementsByClassName("tax-info");
+      let taxInfo = document.getElementsByClassName("tax-info");
       for (let info of taxInfo) {
         info.style.display = info.style.display !== "inline" ? "inline" : "none";
       }
